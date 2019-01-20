01229c308ebe6a6d25ca05109a1d77c2d4ccf2e1
jake <jake@uhuu.com>
2019-01-20 19:51:02 +0100

    first end point

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,19 @@
-console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// cors is needed for going public need good security, otherwise it is a risk
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hellowrold'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
